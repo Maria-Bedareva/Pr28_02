177fdf69bcc6f28d379ae2dd064769d6bd77009f
Maria-Bedareva <82601289+Maria-Bedareva@users.noreply.github.com>
2024-04-05 11:32:37 +0300

    Pr28_02-1.cpp

--- Pr28_02-1.cpp
+++ Pr28_02-1.cpp
@@ -2,10 +2,8 @@
 #include <omp.h>
 
 int main() {
-    // Указываем количество раз, которое нужно вывести приветствие
     int num_times = 5;
 
-    // Директива OpenMP для создания параллельного цикла
 #pragma omp parallel for
     for (int i = 0; i < num_times; ++i) {
         std::cout << "Hello World!" << std::endl;
